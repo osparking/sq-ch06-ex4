450abdab044f341652ad4ac1ebd363a2e095bda0
osparking <jbpark03@gmail.com>
2023-09-16 23:06:22 +0900

    define a comment use case - publish

---
+++ src/main/java/services/CommentService.java
@@ -0,0 +1,18 @@
+package services;
+
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import model.Comment;
+import space.jbpark.utility.MyUtil;
+
+@Service
+public class CommentService {
+	private Logger logger = MyUtil.getLogger(CommentService.class.getName());
+
+	public String publishComment(Comment comment) {
+		logger.info("출판한 코맨트 텍스트:" + comment.getText());
+		return "성공";
+	}
+}
